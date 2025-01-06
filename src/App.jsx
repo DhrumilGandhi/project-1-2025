006c23d3ee2ce83621835556bda4302a414a7b7f
dhrumilgandhi <dhrumilgandhi540@gmail.com>
2025-01-06 15:52:20 +0530

    Add Logo and use props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Hero from './components/Hero';
  
 const App = () => {
   return (
     <>
       <NavBar/>
+      <Hero title="Become a React Dev" subTitle="Find the React job that fits your skills and needs"/>
     </>
   )
 }

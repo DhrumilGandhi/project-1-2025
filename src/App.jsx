b35f1016afa150c9c02d82307311109b7b8d9d98
dhrumilgandhi <dhrumilgandhi540@gmail.com>
2025-01-06 16:16:28 +0530

    use children props and use dynamic class change into one component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
  
 const App = () => {
   return (
     <>
       <NavBar/>
       <Hero title="Become a React Dev" subTitle="Find the React job that fits your skills and needs"/>
+      <HomeCards />
     </>
   )
 }

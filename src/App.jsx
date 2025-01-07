90e6063baa8686a4eaa3cef539f49383516b6674
dhrumilgandhi <dhrumilgandhi540@gmail.com>
2025-01-07 15:46:40 +0530

    use react-route-dom and also use of outlin for layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import React from 'react';
-import NavBar from './components/NavBar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
- 
-const App = () => {
-  return (
-    <>
-      <NavBar/>
-      <Hero title="Become a React Dev" subTitle="Find the React job that fits your skills and needs"/>
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}> 
+      <Route index element={<HomePage />} />
+    </Route>
   )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App
